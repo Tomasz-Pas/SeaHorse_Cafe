00a90fbde38e4602e5e72af9035d9f4f0de39bb7
Tomasz-Pas <123735365+Tomasz-Pas@users.noreply.github.com>
2023-03-21 13:27:29 +0000

    Add files via upload

---
+++ javaScript/contact.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByClassName("button")[0]
+const links = document.getElementsByClassName("nav-links")[0]
+
+button.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
+
+function ClearText(){
+	document.getElementById('textarea').value = '';
+}
